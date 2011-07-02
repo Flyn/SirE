016c37e26886172592b50c68dd4ae36c277c170f
Flyn <flyn.mail@gmail.com>
2011-07-02 23:46:25 +0200

    Added acceleration, friction, up and down to change y position, better collision with the tile and onscreen debug info

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 require "mainchar"
 
 function love.load()
-	-- love.graphics.setMode(1280, 720, false, true, 0)
+	--love.graphics.setMode(1280, 720, false, true, 0)
 	tileImg = love.graphics.newImage("tile.tga")
 	tileImg:setFilter("nearest","nearest")
 	ninjaChar = MainChar.create("ninja")
-	ninjaChar.xpos = 120
+	ninjaChar.xpos = 20
 	ninjaChar.ypos = 100
 	tile = {}
-	tile.xpos = 160
+	tile.xpos = 350
 	tile.ypos = 100
 	zoom = false
 end
@@ -20,9 +20,17 @@ function love.update(dt)
 	if love.keyboard.isDown("right") then
 		ninjaChar:moveRight()
 	end
+	if love.keyboard.isDown("up") then
+		ninjaChar.ypos = ninjaChar.ypos-1
+	end
+	if love.keyboard.isDown("down") then
+		ninjaChar.ypos = ninjaChar.ypos+1
+	end
 	if love.keyboard.isDown("escape") then
 		os.exit(0)
 	end
+	
+	ninjaChar:updatePos()
 	ninjaChar:isBumpingTiles({tile})
 	
 end
@@ -37,8 +45,8 @@ function love.draw()
 	if zoom then
 		love.graphics.scale(2,2)
 	end
-	 love.graphics.print("Ninja physics demo\nLeft/Right to move\nRight Click to zoom\nEscape to quit", 400, 300)
-	 --love.graphics.draw(ninja, xpos, ypos, 0, 1, 1, 20, 20)
+	 love.graphics.print("Ninja physics demo\nLeft/Right to move\nRight Click to zoom\nEscape to quit", 0, 200)
+	 love.graphics.print("Speed:"..ninjaChar.xspd.."\nXpos:"..ninjaChar.xpos.."\nYpos"..ninjaChar.ypos, 200, 200)
 	 love.graphics.line(0,ninjaChar.ypos+4,500,ninjaChar.ypos+4)
 	 ninjaChar:draw()
 	 love.graphics.draw(tileImg, tile.xpos, tile.ypos)
--- mainchar.lua
+++ mainchar.lua
@@ -9,29 +9,64 @@ function MainChar.create(name)
 	newChar.image:setFilter("nearest","nearest")
 	newChar.xpos = 0
 	newChar.ypos = 0
+	newChar.acc = 0.046875
+	newChar.dec = 0.5
+	newChar.xspd = 0
+	newChar.maxSpd = 6
+	newChar.isMoving = false
 	return newChar
 end
 
 function MainChar:moveRight()
-	self.xpos = self.xpos + 1
+	self.isMoving = true
+	if self.xspd < 0 then
+		self.xspd = self.xspd + 0.5
+	elseif self.xspd < self.maxSpd then
+		self.xspd = self.xspd + self.acc
+		if self.xspd > self.maxSpd then
+			self.xspd = self.maxSpd
+		end
+	end
 end
 
 function MainChar:moveLeft()
-	self.xpos = self.xpos - 1
+	self.isMoving = true
+	if self.xspd > 0 then
+		self.xspd = self.xspd - 0.5
+	elseif self.xspd > -self.maxSpd then
+		self.xspd = self.xspd - self.acc
+		if self.xspd < -self.maxSpd then
+			self.xspd = -self.maxSpd
+		end
+	end
+end
+
+function MainChar:updatePos()
+	if not self.isMoving then
+		local sign = 1
+		if self.xspd ~= 0 then
+			sign = (math.abs(self.xspd)/self.xspd)
+		end
+		self.xspd = self.xspd - math.min(math.abs(self.xspd), self.acc) * sign
+	end
+	self.xpos = self.xpos + self.xspd
+	self.isMoving = false
 end
 
 function MainChar:isBumpingTiles(tiles)
 	local sensorY = self.ypos+4
-	local sensorX1 = self.xpos-10
-	local sensorX2 = self.xpos+10
+	local sensorX1 = math.floor(self.xpos-10)
+	local sensorX2 = math.ceil(self.xpos)+10
 	for i,tile in ipairs(tiles) do
 		local tilexpos2 = tile.xpos+16
 		local tileypos2 = tile.ypos+16
 		if sensorY > tile.ypos and sensorY < tileypos2 then
-			if sensorX1 > tile.xpos and sensorX1 < tilexpos2 then
+			if sensorX1 >= tile.xpos and sensorX1 <= tilexpos2 then
 				self.xpos = tilexpos2+11
-			elseif sensorX2 > tile.xpos and sensorX2 < tilexpos2 then
+				self.xspd = 0
+			elseif sensorX2 >= tile.xpos and sensorX2 <= tilexpos2 then
 				self.xpos = tile.xpos-11
+				self.xspd = 0
 			end
 		end
 	end
