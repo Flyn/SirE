b8c77033df4e93aa47581c08dae3f1c0e08d9912
Flyn <flyn.mail@gmail.com>
2011-07-28 21:10:24 +0200

    Corrected collisions when in air.

--- mode_air.lua
+++ mode_air.lua
@@ -70,27 +70,41 @@ function ModeAir:checkForCeiling(tiles)
 	local ceilingSensorBar1 = SensorRect.create(self.character,-sensorray,-sensorray,-(self.character.height/2)-15,0)
 	local ceilingSensorBar2 = SensorRect.create(self.character,sensorray,sensorray,-(self.character.height/2)-15,0)
 	local touchCeiling = false
+	local maxY = nil
+	
 	for i,tile in ipairs(tiles) do
-		if self.character.ypos < (tile.ypos + tile.height -1 + (self.character.height/2)) then
-			local touchCeiling1 = ceilingSensorBar1:isColliding(tile)
-			local touchCeiling2 = ceilingSensorBar2:isColliding(tile)
-			if(touchCeiling1 or touchCeiling2) then
-                if (tile.angle > 90 and tile.angle <= 135) then
-                    self.character.mode = self.character.modeRightWall
-                    self.character.angle = tile.angle
-                    self.character.grndspd = math.max(2,self.character.yspd)*-math.getSign(self.character.yspd*math.cos(math.rad(self.character.angle)))
-                    print(self.character.grndspd)
-                elseif (tile.angle < -90 and tile.angle >= -135) then
-                    self.character.mode = self.character.modeLeftWall
-                    self.character.angle = tile.angle
-                    self.character.grndspd = math.max(2,self.character.yspd)*-math.getSign(self.character.yspd*math.cos(math.rad(self.character.angle)))
-                    print(self.character.grndspd)
-                else
-                    self.character.ypos = tile.ypos + tile.height + 1 + (self.character.height/2)
-                    self.character.yspd = 0
-                end
+		local touchCeiling1, tileypos = ceilingSensorBar1:isColliding(tile)
+		local touchCeiling2, tileypos2 = ceilingSensorBar2:isColliding(tile)
+		if touchCeiling1 then
+			if not maxY then maxY = tileypos end
+			if (tileypos >= maxY) then
+				maxY = tileypos
 			end
-			touchCeiling = touchCeiling or touchCeiling1 or touchCeiling2
+		end
+		if touchCeiling2 then
+			if not maxY then maxY = tileypos2 end
+			if ((tileypos2) >= maxY) then
+				maxY = tileypos2
+			end
+		end
+		if(touchCeiling1 or touchCeiling2) then
+			if (not tile.flipy) then
+				maxY = tile.ypos + tile.height
+			end
+			if self.character.ypos < (maxY + (self.character.height/2)) then
+				if (tile.angle > 90 and tile.angle <= 135) then
+					self.character.mode = self.character.modeCeiling
+					self.character.angle = tile.angle
+					self.character.grndspd = self.character.yspd*-math.getSign(math.cos(math.rad(self.character.angle)))
+				elseif (tile.angle < -90 and tile.angle >= -135) then
+					self.character.mode = self.character.modeCeiling
+					self.character.angle = tile.angle
+					self.character.grndspd = self.character.yspd*-math.getSign(math.cos(math.rad(self.character.angle)))
+				else
+					self.character.ypos = maxY + (self.character.height/2)
+					self.character.yspd = 0
+				end
+			end
 		end
 	end
 end
@@ -108,26 +122,26 @@ function ModeAir:checkForGround(tiles)
 		for i,tile in ipairs(tiles) do
 			local onGround1 = false
 			local onGround2 = false
-			if self.character.ypos > tile.ypos-(self.character.height/2) then
-				local tileypos = false
-				local tileypos2 = false
-				onGround1, tileypos = groundSensorBar1:isColliding(tile)
-				onGround2, tileypos2 = groundSensorBar2:isColliding(tile)
-				if onGround1 then
-					if not minY then minY = tileypos end
-					if (tileypos <= minY) then
-						minY = tileypos
-						self.character.angle = tile.angle
-					end
+			local tileypos = false
+			local tileypos2 = false
+			onGround1, tileypos = groundSensorBar1:isColliding(tile)
+			onGround2, tileypos2 = groundSensorBar2:isColliding(tile)
+			if onGround1 then
+				if not minY then minY = tileypos end
+				if (tileypos <= minY) then
+					minY = tileypos
+					self.character.angle = tile.angle
 				end
-				if onGround2 then
-					if not minY then minY = tileypos2 end
-					if ((tileypos2) <= minY) then
-						minY = tileypos2
-						self.character.angle = tile.angle
-					end
+			end
+			if onGround2 then
+				if not minY then minY = tileypos2 end
+				if ((tileypos2) <= minY) then
+					minY = tileypos2
+					self.character.angle = tile.angle
 				end
-				if onGround1 or onGround2 then
+			end
+			if onGround1 or onGround2 then
+				if self.character.ypos > minY-(self.character.height/2) then
 					self.character.ypos = minY-(self.character.height/2)
 					if self.character.angle > -20 and self.character.angle < 20 then
 						self.character.grndspd = self.character.xspd
@@ -147,7 +161,7 @@ function ModeAir:checkForGround(tiles)
 	                self.character:setOnFloor()
 					self.character:unroll()
 				end
-	        end
+			end
 	        onGround = onGround or onGround1 or onGround2
 	    end
     end
